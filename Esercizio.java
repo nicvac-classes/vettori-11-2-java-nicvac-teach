0667e6f35374ec23c64d0aaddea5e7da3eca163c
nicvac-teach <nicola.vacca@marconibari.edu.it>
2025-01-01 21:31:45 +0000

    Implementazione

--- Esercizio.java
+++ Esercizio.java
@@ -1,6 +1,3 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
-
-//Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
 
 // Classe principale, con metodo main
@@ -8,19 +5,46 @@ class Esercizio {
     // Il programma parte con una chiamata a main().
     public static void main(String args[])
     {
-        //Variabili del programma
-        String nome;
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        int i;
+        int j;
+        boolean duplicato;
+        int contatore;
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
-
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
-
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        System.out.println("Inserisci la dimensione del vettore");
+        n = Integer.parseInt(scanner.nextLine());
+        
+        // Dichiara e inizializza il vettore convenzionale
+        int[] v = new int[n];
+        
+        // Input dei valori
+        for (i = 0; i <= n-1; i++) {
+            System.out.print("Inserisci il " + (i + 1) + "° valore: ");
+            v[i] = Integer.parseInt(scanner.nextLine());
+        }
+        
+        // Ricerca dei duplicati
+        for (i = 0; i <= n-2; i++) {
+            duplicato = false;
+            contatore = 1;
+            
+            for (j = i + 1; j <= n-1; j++) {
+                if (v[i] == v[j]) {
+                    System.out.println("Il valore " + v[i] + " occorre nelle posizioni " + i + " e " + j);
+                    contatore = contatore + 1;
+                    duplicato = true;
+                }
+            }
+            
+            if (duplicato) {
+                System.out.println("E' stato inserito " + contatore + " volte.");
+            }
+        }
+        
+        scanner.close();
     }
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
+
