349c2245284c39bde0965c7fbc04f466ca10ef3e
nicvac-teach <nicola.vacca@marconibari.edu.it>
2025-01-01 21:33:05 +0000

    indentazione

--- Esercizio.java
+++ Esercizio.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 // Classe principale, con metodo main
 class Esercizio {
     // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         int n;
         int i;
@@ -14,37 +13,34 @@ class Esercizio {
 
         System.out.println("Inserisci la dimensione del vettore");
         n = Integer.parseInt(scanner.nextLine());
-        
+
         // Dichiara e inizializza il vettore convenzionale
         int[] v = new int[n];
-        
+
         // Input dei valori
-        for (i = 0; i <= n-1; i++) {
+        for (i = 0; i <= n - 1; i++) {
             System.out.print("Inserisci il " + (i + 1) + "° valore: ");
             v[i] = Integer.parseInt(scanner.nextLine());
         }
-        
+
         // Ricerca dei duplicati
-        for (i = 0; i <= n-2; i++) {
+        for (i = 0; i <= n - 2; i++) {
             duplicato = false;
             contatore = 1;
-            
-            for (j = i + 1; j <= n-1; j++) {
+
+            for (j = i + 1; j <= n - 1; j++) {
                 if (v[i] == v[j]) {
                     System.out.println("Il valore " + v[i] + " occorre nelle posizioni " + i + " e " + j);
                     contatore = contatore + 1;
                     duplicato = true;
                 }
             }
-            
+
             if (duplicato) {
                 System.out.println("E' stato inserito " + contatore + " volte.");
             }
         }
-        
+
         scanner.close();
     }
 }
-
-
-
